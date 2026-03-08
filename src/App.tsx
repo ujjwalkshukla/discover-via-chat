1141b0c698afc46f2ff5293a1b7f8567ad4bec48
Ujjwal Kumar Shukla <100704162+ujjwalkshukla@users.noreply.github.com>
2026-03-08 13:49:38 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -15,7 +15,7 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
-      <BrowserRouter>
+      <BrowserRouter basename="/discover-via-chat/">
         <Routes>
           <Route path="/" element={<ChatPage />} />
           <Route path="/video/:id" element={<VideoPlayerPage />} />
